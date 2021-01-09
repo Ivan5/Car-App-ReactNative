0f5ac8a06a2c60823061eb305f43cc0766c16ba6
Ivan5 <converse_ivan@hotmail.com>
2021-01-08 18:14:30 -0600

    Make a render items from other file

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CarItem from "./components/CarItem";
+import CarList from "./components/CarList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <CarList />
       <StatusBar style="auto" />
     </View>
   );
